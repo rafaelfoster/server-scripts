9287f4b161082fcf7d52c44c9e65e9c8c4ff9bd1
Rafael Graciano Foster <rfoster@rodrimar.com.br>
2015-01-22 16:32:37 -0200

    Creating python test script to read YAML config file

---
+++ linux/python/python-yaml.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import yaml
+
+stream = open("config.yml", 'r')
+yml_config = yaml.load(stream)
+
+for section_key, section_value in yml_config.items():
+	print "Sessao: " + section_key
+	print ""
+	for configkey, configvalue in section_value.items():
+		print "%s -> %s" % (configkey, configvalue)
+	print "----------------------------------------------------"
