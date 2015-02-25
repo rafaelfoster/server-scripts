717a0fbcdd9ffb8b10655407345a011ec9103b5d
Rafael Foster <rafaelgfoster@gmail.com>
2015-02-25 13:24:40 -0300

    Fix the merge request #3 - Removing all variables after finish the backup (avoid another job to use   and variable from other job) - Improve readability with skipping the default section

--- linux/python/pyBackupMysql.py
+++ linux/python/pyBackupMysql.py
@@ -15,166 +15,179 @@ import datetime
 from subprocess import Popen, PIPE
 import shlex
 
-configFile = "config.yml"
 count = 0
 logfile = None
 jobname = None
 log_file = None
+ssh_bin = "/usr/bin/ssh"
+mysql_bin = "/usr/bin/mysql"
+mysqldump_bin = "/usr/bin/mysqldump"
+configFile = "/path/to/config/config.yml"
 
 def main():
+
 	strError = list()
 	stream = open(configFile, 'r')
 	yml_config = yaml.load(stream)
+	
+
+	print yml_config
 
 	for section_key, section_value in yml_config.items():
 		global jobname
 		jobname = section_key
 		cmd_ssh_proxy = None
-		
+
 		tmpdebugVars = list();
 		for configkey, configvalue in section_value.items():
 			tmpdebugVars.append("Definindo variavel: %s -> %s " % (configkey, configvalue) )
 			globals()[configkey] = configvalue
-
+				
 		if "log_file_folder" in globals():
 			for strToLog in tmpdebugVars:
-			 _WriteOutput(strToLog)
+				_WriteOutput(strToLog)
 
-		if jobname != "default":
-			hostname = db_hostname
-			if checkVar('db_ssh_addr'):
-				hostname = db_ssh_addr
-			dbbkppath = db_bkp_path + "/" + hostname
-			
-			_WriteOutput("Starting job: " + jobname)
-			_WriteOutput("Backup folder: " + db_bkp_path)
+		# Skip if the section is default
+		if jobname == "default": continue;
+	
+		hostname = db_hostname
+		if checkVar('db_ssh_addr'):
+			hostname = db_ssh_addr
+		dbbkppath = db_bkp_path + "/" + hostname
+		
+		_WriteOutput("Starting job: " + jobname)
+		_WriteOutput("Backup folder: " + db_bkp_path)
 
-			if dbbkppath and not os.path.exists(dbbkppath):
-				os.makedirs(dbbkppath)
-			os.chdir(dbbkppath)
+		if dbbkppath and not os.path.exists(dbbkppath):
+			os.makedirs(dbbkppath)
+		os.chdir(dbbkppath)
 
-			if checkVar('db_ssh_addr') and checkVar('db_ssh_user'):
-				cmd_ssh_proxy = "ssh %s@%s" % (db_ssh_user, db_ssh_addr)
+		if db_hostname and db_username and db_password:
+			cmd_mysql_listdb = "%s -h %s -u %s -p%s -e 'SHOW DATABASES;' --skip-column-names" % (mysql_bin, db_hostname, db_username, db_password)
 
-			if db_hostname and db_username and db_password:
-				cmd_mysql_listdb = "mysql -h %s -u %s -p%s -e 'SHOW DATABASES;' --skip-column-names" % (db_hostname, db_username, db_password)
+		if checkVar('db_ssh_addr') and checkVar('db_ssh_user'):
+			cmd_ssh_proxy = "%s %s@%s" % (ssh_bin, db_ssh_user, db_ssh_addr)
+			cmd = '%s "%s"' % ( cmd_ssh_proxy, cmd_mysql_listdb )
+			_WriteOutput("Use SSH proxy: " + cmd_ssh_proxy )
+		else:
+			cmd = cmd_mysql_listdb
 
-			if cmd_ssh_proxy:
-				cmd = shlex.split(cmd_ssh_proxy) + [ cmd_mysql_listdb ]
-			else:
-				cmd = shlex.split(cmd_mysql_listdb)
+		if db_hostname and db_username and db_password:
+			p1 = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+			dbs, p1_error  = p1.communicate()
 
-			if db_hostname and db_username and db_password:
-				p1 = Popen(cmd, stdout=PIPE, stderr=PIPE)
-				dbs, p1_error  = p1.communicate()
+			if checkVar("db_filter") == True:
+				dbsBackup = list()
+				for dbfilter in db_filter.split():
+					if dbfilter in dbs:
+						dbsBackup.append(dbfilter)
+				dbs = '\n'.join(dbsBackup)
+			elif checkVar("db_exception") == True:
+				exception = re.sub(" ","|", db_exception)
+				dbs = re.sub( r'%s' % exception, "", dbs)
 
-				if checkVar("db_filter") == True:
-					dbsBackup = list()
-					for dbfilter in db_filter.split():
-						if dbfilter in dbs:
-							dbsBackup.append(dbfilter)
-					dbs = '\n'.join(dbsBackup)
-				elif checkVar("db_exception") == True:
-					exception = re.sub(" ","|", db_exception)
-					dbs = re.sub( r'%s' % exception, "", dbs)
+			if not dbs:
+				if p1_error: 
+					_WriteOutput("Error: " + p1_error + "\n Database list is empty.")
+					strError.append(p1_error)
+					strError.append("Database list is empty.")
+					break
+				sendMail(jobname, "Execution error: %s <br> Error: %s" % (strError, p1_error) )
+				continue
+		# End If db_hostname
 
-				if not dbs:
-					if p1_error: 
-						_WriteOutput("Error: " + p1_error + "\n Database list is empty.")
-						strError.append(p1_error)
-						strError.append("Database list is empty.")
-						break
-					sendMail(jobname, "Execution error: %s <br> Error: %s" % (strError, p1_error) )
+		dblist = re.split("\s+", dbs)
+
+		jobStartTime = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+		msg = '''<p><b> Job Start time: </b>%s </p>
+		<table border='1' style='border: 1px solid gray;'>
+			<tr align='center' style='background-color: #D9D9D9;'>
+				<td><b>Database</b></td>
+				<td><b>Size</b></td>
+				<td><b>SHA1</b></td>
+				<td><b>Time</b></td>
+			</tr> 
+		''' % (jobStartTime)
+
+		for dbname in dblist:
+			if len(dbname) > 0:
+				fdbname_zip = dbname + ".sql.gz"
+
+				cmd_mysqldump = "%s --single-transaction --routines --quick -h %s -u %s -p%s -B %s " % (mysqldump_bin, db_hostname, db_username, db_password, dbname)
+				if cmd_ssh_proxy:
+					cmd = '%s "%s"' % ( cmd_ssh_proxy, cmd_mysqldump )
+				else:
+					cmd = cmd_mysqldump
+
+				_WriteOutput("Command to Exec: " + cmd)
+				strBkpStart = int(time.time())
+
+				if os.path.exists(fdbname_zip):
+					_WriteOutput("File already exist, renaming to .old")
+					os.rename(fdbname_zip, fdbname_zip + ".old")
+
+				try:
+					_WriteOutput("Creating zip file...")
+					oFid = gzip.open(fdbname_zip, 'wb')
+					sort = Popen(shlex.split(cmd), stdout=PIPE)
+					oFid.writelines(sort.stdout)
+					oFid.close()
+				except IOError as err:
+					_WriteOutput("Error: %s" % (err))
+					os.remove( fdbname_zip )
+					os.rename(fdbname_zip + ".old", fdbname_zip)
 					continue
-			# End If db_hostname
-
-			dblist = re.split("\s+", dbs)
-
-			jobStartTime = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-			msg = '''<p><b> Job Start time: </b>%s </p>
-			<table border='1' style='border: 1px solid gray;'>
-				<tr align='center' style='background-color: #D9D9D9;'>
-					<td><b>Database</b></td>
-					<td><b>Size</b></td>
-					<td><b>SHA1</b></td>
-					<td><b>Time</b></td>
-				</tr> 
-			''' % (jobStartTime)
-
-			for dbname in dblist:
-				if len(dbname) > 0:
-					fdbname_zip = dbname + ".sql.gz"
-
-					cmd_mysqldump = "mysqldump --single-transaction --routines --quick -h %s -u %s -p%s -B %s " % (db_hostname, db_username, db_password, dbname)
-					if cmd_ssh_proxy:
-						cmd = shlex.split(cmd_ssh_proxy) + [ cmd_mysqldump ]
-					else:
-						cmd = shlex.split(cmd_mysqldump)
-
-					_WriteOutput("Command to Exec: " + cmd_mysqldump)
-					strBkpStart = int(time.time())
+				else:
+					_WriteOutput("Writing dump to file: OK")
+					if os.path.exists(fdbname_zip + ".old"):
+						_WriteOutput("Deleting old backup file...")
+						os.remove( fdbname_zip + ".old" )
 
 					if os.path.exists(fdbname_zip):
-						_WriteOutput("File already exist, renaming to .old")
-						os.rename(fdbname_zip, fdbname_zip + ".old")
-
-					try:
-						_WriteOutput("Creating zip file...")
-						oFid = gzip.open(fdbname_zip, 'wb')
-						sort = Popen(cmd, stdout=PIPE)
-						oFid.writelines(sort.stdout)
-						oFid.close()
-					except IOError as err:
-						_WriteOutput("Error: %s" % (err))
-						os.remove( fdbname_zip )
-						os.rename(fdbname_zip + ".old", fdbname_zip)
-						continue
+						fgetsize = os.path.getsize(fdbname_zip)
+						uid = pwd.getpwnam(fUserid).pw_uid
+						gid = grp.getgrnam(fGroupid).gr_gid
+						os.chown(fdbname_zip, uid, gid )
+						dbsha1sum = getHash(fdbname_zip)
+						_WriteOutput("Backup file size: %s " % (fgetsize) )
 					else:
-						_WriteOutput("Writing dump to file: OK")
-						if os.path.exists(fdbname_zip + ".old"):
-							_WriteOutput("Deleting old backup file...")
-							os.remove( fdbname_zip + ".old" )
+						_WriteOutput("Some error occur. Backup File was not found!")
+						msg = msg + "<tr><td> %s </td><td colspan='2'>  Backup file not found! </td></tr>" % (dbname)
+						continue
 
-						if os.path.exists(fdbname_zip):
-							fgetsize = os.path.getsize(fdbname_zip)
-							uid = pwd.getpwnam(fUserid).pw_uid
-							gid = grp.getgrnam(fGroupid).gr_gid
-							os.chown(fdbname_zip, uid, gid )
-							dbsha1sum = getHash(fdbname_zip)
-							_WriteOutput("Backup file size: %s " % (fgetsize) )
-						else:
-							_WriteOutput("Some error occur. Backup File was not found!")
-							msg = msg + "<tr><td> %s </td><td colspan='2'>  Backup file not found! </td></tr>" % (dbname)
-							continue
+					dbsize = convertSize(fgetsize)
+					strBkpEnd = int(time.time())
+					d = divmod(strBkpEnd - strBkpStart, 86400)
+					h = divmod(d[1],3600)
+					m = divmod(h[1],60)
+					s = m[1]
+					timespend = '%d hours, %d minutes, %d seconds' % (h[0],m[0],s)
 
-						dbsize = convertSize(fgetsize)
-						strBkpEnd = int(time.time())
-						d = divmod(strBkpEnd - strBkpStart, 86400)
-						h = divmod(d[1],3600)
-						m = divmod(h[1],60)
-						s = m[1]
-						timespend = '%d hours, %d minutes, %d seconds' % (h[0],m[0],s)
+					_WriteOutput("Backup Finished after " + timespend)
+					msg = msg + '''
+						<tr>
+							<td> %s </td>
+							<td align='right'> %s </td>
+							<td> %s </td>
+							<td> %s </td>
+						</tr>
+						''' % (dbname, dbsize, dbsha1sum, timespend)
+		# End for dbname
 
-						_WriteOutput("Backup Finished after " + timespend)
-						msg = msg + '''
-							<tr>
-								<td> %s </td>
-								<td align='right'> %s </td>
-								<td> %s </td>
-								<td> %s </td>
-							</tr>
-							''' % (dbname, dbsize, dbsha1sum, timespend)
-			# End for dbname
+		jobEndTime = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+		msg = msg + '''</table> <p><b> Job Finished: </b> %s </p> ''' % (jobEndTime)
+		sendMail(jobname, msg)
+		global logfile
+		_WriteOutput("\n------------------------\n")
+		logfile.close()
 
-			jobEndTime = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-			msg = msg + '''</table> <p><b> Job Finished: </b> %s </p> ''' % (jobEndTime)
-			sendMail(jobname, msg)
-			global logfile
-			_WriteOutput("\n------------------------\n")
-			logfile.close()
-		# end If not Default
+		for configkey, configvalue in section_value.items():
+			tmpdebugVars.append("Removendo variavel: %s -> %s " % (configkey, configvalue) )
+			del globals()[configkey]
+
 	# End For section_key
 # End Main()
+
 def convertSize(size):
 	size = ( size / 1024 )
 	return "%s %s" % (size, "Kb")
